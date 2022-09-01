8a3b5475f69012f0798c9e4095c63c792e59b1d3
Paul Groskopf <paul@grskopf-embedded.com>
2022-09-01 19:43:42 +0200

    Corrected image names

---
+++ push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+if [ -z "$CR_PAT" ]; then
+  echo "CR_PAT environment variable is not set"
+  exit 1;
+fi
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+docker-compose push
